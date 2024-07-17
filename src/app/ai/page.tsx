f1bc649bc4c283deb297909b181d73d82c569cfc
“suphanan27git config --global user.name “suphanan27 <suphanankonsuay@gmail.com>
2024-07-17 15:39:13 +0700

    first commit

---
+++ src/app/ai/page.tsx
@@ -0,0 +1,10 @@
+import AI from '@/components/Ai'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <AI/>
+    </div>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Main from '@/components/Main'
+
+export default function home() {
+  return (
+    <div>
+      <Main/>
+    </div>
+  )
+}
